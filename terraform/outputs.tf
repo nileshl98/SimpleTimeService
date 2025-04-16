e3f63dc8bcfc3410eaad99bdf99c6b88b053ea59
nilesh <niesh@gmail.com>
2025-04-16 15:53:00 +0530

    changed

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
-output "load_balancer_dns_name" {
-  value = aws_lb.this.dns_name
+output "eks_cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
 }
+
+output "eks_cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+
+::contentReference[oaicite:0]{index=0}
+ 
