eb831cb39b696d2bde3b4d678d5aac78b054036b
nilesh <niesh@gmail.com>
2025-04-16 15:53:05 +0530

    changed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,31 @@
 variable "aws_region" {
-  default = "us-east-1"
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  default     = "simpletimeservice-cluster"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "container_image" {
-  description = "Docker image"
+  description = "Docker image for the application"
+  default     = "your_dockerhub_username/simpletimeservice:latest"
 }
