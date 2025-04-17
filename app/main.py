81779c69799adf62034152ef958d2594fad0802e
nileshl98 <nileshlipane98@gmail.com>
2025-04-17 14:08:43 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 from datetime import datetime
+import socket
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+@app.route('/')
+def home():
+    ip = request.remote_addr
     return jsonify({
-        "timestamp": datetime.utcnow().isoformat(),
-        "ip": request.remote_addr
+        "timestamp": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC'),
+        "ip": ip
     })
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
