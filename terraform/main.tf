76d64bbb01e8bdff45f7a80e4ad9d5df48eab6fe
nileshl98 <nileshlipane98@gmail.com>
2025-04-17 14:17:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,122 +1,35 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.83.0"
-    }
-  }
-}
-
-provider "aws" {
-  region     = var.aws_region
-  access_key = "AKIAZ3MGNADJJH6YHU"
-  secret_key = "lzu35aP3QamVI9BMt6o44tAbeewGCem4V"
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"
+  version = "4.0.2"
 
   name = "simpletimeservice-vpc"
   cidr = var.vpc_cidr
 
-  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
+  azs             = ["ap-south-1a", "ap-south-1b"]
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
   enable_dns_hostnames = true
-
-  tags = {
-    Terraform = "true"
-    Project   = "SimpleTimeService"
-  }
+  enable_dns_support   = true
 }
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "20.35.0"
   cluster_name    = var.cluster_name
   cluster_version = "1.27"
-  subnet_ids      = module.vpc.private_subnets
+  subnets         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
-  enable_irsa = true
+  manage_aws_auth = true
+  enable_irsa     = true
 
-  eks_managed_node_group_defaults = {
-    instance_types = ["t3.medium"]
-  }
-
-  eks_managed_node_groups = {
-    default = {
+  node_groups = {
+    simpletimeservice_nodes = {
       desired_capacity = 2
-      min_capacity     = 1
       max_capacity     = 3
+      min_capacity     = 1
+
+      instance_types = ["t3.small"]
     }
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
-}
-
-resource "kubernetes_deployment" "simpletimeservice" {
-  metadata {
-    name      = "simpletimeservice"
-    namespace = "default"
-    labels = {
-      app = "simpletimeservice"
-    }
-  }
-
-  spec {
-    replicas = 2
-
-    selector {
-      match_labels = {
-        app = "simpletimeservice"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "simpletimeservice"
-        }
-      }
-
-      spec {
-        container {
-          name  = "simpletimeservice"
-          image = var.container_image
-
-          port {
-            container_port = 8080
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "simpletimeservice" {
-  metadata {
-    name      = "simpletimeservice"
-    namespace = "default"
-  }
-
-  spec {
-    selector = {
-      app = "simpletimeservice"
-    }
-
-    port {
-      port        = 80
-      target_port = 8080
-    }
-
-    type = "LoadBalancer"
   }
 }
