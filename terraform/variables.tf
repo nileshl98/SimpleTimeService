0f243b8860d7bf6adb4b970095c80ee3aa643288
nileshl98 <nileshlipane98@gmail.com>
2025-04-17 14:14:19 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,25 @@
 variable "aws_region" {
-  description = "AWS region"
-  default     = "ap-south-1"
+  default = "ap-south-1"
 }
 
 variable "cluster_name" {
-  description = "EKS cluster name"
-  default     = "simpletimeservice-cluster"
+  default = "simpletimeservice-eks"
 }
 
 variable "vpc_cidr" {
-  description = "VPC CIDR block"
-  default     = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnets" {
-  description = "Public subnet CIDRs"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets" {
-  description = "Private subnet CIDRs"
-  type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+  type    = list(string)
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
-variable "container_image" {
-  description = "Docker image for the application"
-  default     = "nilipane23/simpletimeservice:latest"
+variable "docker_image" {
+  default = "nilipane23/simpletimeservice:latest"
 }
